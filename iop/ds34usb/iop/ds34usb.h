542715c4b86136b3f925b91b9c43be474fd9acd2
bulatorr <84269101+bulatorr@users.noreply.github.com>
2025-05-07 19:00:03 +0300

    Update ds34usb.h
    maybe dualsense support

--- iop/ds34usb/iop/ds34usb.h
+++ iop/ds34usb/iop/ds34usb.h
@@ -6,7 +6,7 @@
 #define DS34_VID     0x054C  // Sony Corporation
 #define DS3_PID      0x0268  // PS3 Controller
 #define DS4_PID      0x05C4  // PS4 Controller
-#define DS4_PID_SLIM 0x09CC  // PS4 Slim Controller
+#define DS4_PID_SLIM 0x0CE6  // PS4 Slim Controller
 
 #define DS3 0
 #define DS4 1
@@ -131,55 +131,56 @@ struct ds3report
 struct ds4report
 {
     u8 ReportID;
-    u8 LeftStickX;    // left Joystick X axis 0 - 255, 128 is mid
-    u8 LeftStickY;    // left Joystick Y axis 0 - 255, 128 is mid
-    u8 RightStickX;   // right Joystick X axis 0 - 255, 128 is mid
-    u8 RightStickY;   // right Joystick Y axis 0 - 255, 128 is mid
-    u8 Dpad     : 4;  // hat format, 0x08 is released, 0=N, 1=NE, 2=E, 3=SE, 4=S, 5=SW, 6=W, 7=NW
-    u8 Square   : 1;
-    u8 Cross    : 1;
-    u8 Circle   : 1;
-    u8 Triangle : 1;
-    u8 L1       : 1;
-    u8 R1       : 1;
-    u8 L2       : 1;
-    u8 R2       : 1;
-    u8 Share    : 1;
-    u8 Option   : 1;
-    u8 L3       : 1;
-    u8 R3       : 1;
-    u8 PSButton : 1;
-    u8 TPad     : 1;
-    u8 Counter1 : 6;  // counts up by 1 per report
-    u8 PressureL2;    // digital Pad L2 button Pressure 0 - 255
-    u8 PressureR2;    // digital Pad R2 button Pressure 0 - 255
-    u8 Counter2;
-    u8 Counter3;
-    u8 Battery;  // battery level from 0x00 to 0xff
+    u8 LeftStickX;
+    u8 LeftStickY;
+    u8 RightStickX;
+    u8 RightStickY;
+    u8 PressureL2;
+    u8 PressureR2;
+    u8 Counter1;
+    u8 Dpad       : 4;
+    u8 Square     : 1;
+    u8 Cross      : 1;
+    u8 Circle     : 1;
+    u8 Triangle   : 1;
+    u8 L1         : 1;
+    u8 R1         : 1;
+    u8 L2         : 1;
+    u8 R2         : 1;
+    u8 Share      : 1;
+    u8 Option     : 1;
+    u8 L3         : 1;
+    u8 R3         : 1;
+    u8 PSButton   : 1;
+    u8 TPad       : 1;
+    u8 Microphone : 1;
+    u8 Reserved2  : 1; // UNK1
+    u8 Reserved3  : 4; // DualSense Edge buttons
+    u8 Reserved4;      // UNK2
+    u32 Counter2;      // UNK_COUNTER
+    s16 GyroX;
+    s16 GyroY;
+    s16 GyroZ;
     s16 AccelX;
     s16 AccelY;
     s16 AccelZ;
-    s16 GyroZ;
-    s16 GyroY;
-    s16 GyroX;
-    u8 Reserved1[5];     // Unknown
-    u8 Power       : 4;  // from 0x0 to 0xA - charging, 0xB - charged
+    u32 SensorTimestamp;     // sensorTimestamp
+    u8 Temperature;          // Temperature
+    u32 Finger1ID      : 7;  // counter
+    u32 nFinger1Active : 1;  // 0 - active, 1 - unactive
+    u32 Finger1X       : 12; // finger 1 coordinates resolution 1920x943
+    u32 Finger1Y       : 12;
+    u32 Finger2ID      : 7;
+    u32 nFinger2Active : 1;
+    u32 Finger2X       : 12; // finger 2 coordinates resolution 1920x943
+    u32 Finger2Y       : 12;
+    u8 Reserved7[12];   // Adaptive triggers
+    u8 Battery     : 4; // battery level from 0x00 to 0xff
+    u8 Power       : 4; // from 0x0 to 0xA - charging, 0xB - charged
+    u8 Reserved8   : 4; // PowerState
     u8 Usb_plugged : 1;
-    u8 Headphones  : 1;
-    u8 Microphone  : 1;
-    u8 Padding     : 1;
-    u8 Reserved2[2];        // Unknown
-    u8 TPpack;              // number of trackpad packets (0x00 to 0x04)
-    u8 PackCounter;         // packet counter
-    u8 Finger1ID     : 7;   // counter
-    u8 Finger1Active : 1;   // 0 - active, 1 - unactive
-    u16 Finger1X     : 12;  // finger 1 coordinates resolution 1920x943
-    u16 Finger1Y     : 12;
-    u8 Finger2ID     : 7;
-    u8 Finger2Active : 1;
-    u16 Finger2X     : 12;  // finger 2 coordinates resolution 1920x943
-    u16 Finger2Y     : 12;
-
+    u8 Reserved9   : 3;
+    u8 Reserved10[9];
 } __attribute__((packed));
 
 void mips_memcpy(void *, const void *, unsigned);
